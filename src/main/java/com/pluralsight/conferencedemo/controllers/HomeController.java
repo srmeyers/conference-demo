6d29c7168dfab06d6854de241ee09c371813fe6b
Samuel Meyers <srmeyers@gmail.com>
2021-02-17 22:22:27 -0600

    Add bob

--- src/main/java/com/pluralsight/conferencedemo/controllers/HomeController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/HomeController.java
@@ -4,12 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class HomeController {
---
+++ src/main/java/com/pluralsight/conferencedemo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.pluralsight.conferencedemo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${app.user.password}")
+    private String userPassword;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "*/js/*").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails bobUserDetails = User.builder()
+                .username("bob")
+                .password(passwordEncoder.encode(userPassword))
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                bobUserDetails
+        );
+    }
+}
